508945c722be394fd6939287dba7d9f7fb9146d6
Secure Cap <Twilliams397tw@gmail.com>
2018-09-10 14:30:13 -0500

    final commit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,32 @@
-const express = require('express');
+/*
 
-const router = express.Router();
+Your unique user ID:
+Api key: 0bcc8225757ff024f45d0b16c6718031
+Api secret: e709b11b283d4dcc
 
-router.get("/api/gallery", (req, res) => {
+*/
+var express = require('express');
+
+var router = express.Router();
+
+var Flickr = require("flickrapi"),
+    flickrOptions = {
+      api_key: "0bcc8225757ff024f45d0b16c6718031",
+      secret: "e709b11b283d4dcc",
+      requestOptions: {
+        timeout: 20000,
+        /* other default options accepted by request.defaults */
+      }
+    };
+
+Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+  // we can now use "flickr" as our API object,
+  // but we can only call public methods and access public data
+});
+
+router.get("/api/gallery",(req, res) => {
     res.send("muahahahaha TO BE DEVELOPED!!")
 });
+
 
 module.exports = router;
