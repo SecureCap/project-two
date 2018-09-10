4712f1bdde6c86440369dd1c7807d69853ebb693
Abraham Ibarra <aibarra988@gmail.com>
2018-09-10 11:15:58 -0500

    added api router

---
+++ routes/api-routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/api/gallery", (req, res) => {
+    res.send("muahahahaha TO BE DEVELOPED!!")
+});
+
+module.exports = router;
