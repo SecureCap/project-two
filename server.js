6a19c63413a5df13580e4d6542a807ffb1dd6123
SuzanneRashka <suzanne.rashka@gmail.com>
2018-09-12 21:14:44 -0500

    linked css/ found logo

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const passport = require('./config/passport');
 const cors = require('cors');
 const helmet = require('helmet');
 const db = require('./models');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 
@@ -22,18 +23,28 @@ app.use(helmet());
 app.use(cors());
 
 // Serve static content from the public directory
-app.use(express.static('/public'));
+// app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Setup body-parser
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 // Setup handlebars
-app.engine('.hbs', exphbs({defaultLayout: '../../public/index', extname: '.hbs'}));
+app.engine('.hbs', exphbs({
+    defaultLayout: '../../public/index',
+    extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 // Setup sessions
-app.use(session({ secret: "lolsecret", resave: true, saveUninitialized: true}));
+app.use(session({
+    secret: "lolsecret",
+    resave: true,
+    saveUninitialized: true
+}));
 
 // Setup Passport
 app.use(passport.initialize());
@@ -45,7 +56,10 @@ app.use(htmlRoutes);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`) });
+db.sequelize.sync({
+    force: true
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`)
+    });
 });
-
